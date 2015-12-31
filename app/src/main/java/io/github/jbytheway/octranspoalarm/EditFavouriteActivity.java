c64c00c4d8b9933f5a0a2b8bf091af0a14b89ebd
John Bytheway <jbytheway@gmail.com>
2015-12-31 20:14:32 +0000

    Actually get the ActionBar working for Edit

--- app/src/main/java/io/github/jbytheway/octranspoalarm/EditFavouriteActivity.java
+++ app/src/main/java/io/github/jbytheway/octranspoalarm/EditFavouriteActivity.java
@@ -2,15 +2,19 @@ package io.github.jbytheway.octranspoalarm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class EditFavouriteActivity extends Activity {
+public class EditFavouriteActivity extends AppCompatActivity {
     public static final String NEW_FAVOURITE = "new_favourite";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_favourite);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
