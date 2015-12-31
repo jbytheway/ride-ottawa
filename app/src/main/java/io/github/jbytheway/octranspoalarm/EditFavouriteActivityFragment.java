e25c701bb57528693216bbb5abe099fc896bbdb4
John Bytheway <jbytheway@gmail.com>
2015-12-31 21:47:43 +0000

    Some stub ListViews

--- app/src/main/java/io/github/jbytheway/octranspoalarm/EditFavouriteActivityFragment.java
+++ app/src/main/java/io/github/jbytheway/octranspoalarm/EditFavouriteActivityFragment.java
@@ -2,14 +2,19 @@ package io.github.jbytheway.octranspoalarm;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class EditFavouriteActivityFragment extends Fragment {
+
+    private static final String TAG = "EditFavouriteFragment";
 
     public EditFavouriteActivityFragment() {
     }
@@ -17,6 +22,25 @@ public class EditFavouriteActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_favourite, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_favourite, container, false);
+        mStopRouteList = (ListView) view.findViewById(R.id.stop_route_list_view);
+
+        // For now, we have a hardcoded string array for our list, for testing
+        String[] array = new String[4];
+        array[0] = "Hello";
+        array[1] = "Hello";
+        array[2] = "Hello";
+        array[3] = "Hello";
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, array);
+        mStopRouteList.setAdapter(adapter);
+        Log.i(TAG, "Added adapter");
+        return view;
     }
+
+    @Override
+    public void onDestroyView() {
+        mStopRouteList = null;
+    }
+
+    private ListView mStopRouteList;
 }
--- app/src/main/java/io/github/jbytheway/octranspoalarm/ListFavouritesActivityFragment.java
+++ app/src/main/java/io/github/jbytheway/octranspoalarm/ListFavouritesActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,22 @@ public class ListFavouritesActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list_favourites, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_favourites, container, false);
+        mFavouriteList = (ListView) view.findViewById(R.id.favourite_list_view);
+
+        // For now, we have a hardcoded string array for our list, for testing
+        String[] array = new String[1];
+        array[0] = "Hello";
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, array);
+        mFavouriteList.setAdapter(adapter);
+
+        return view;
     }
+
+    @Override
+    public void onDestroyView() {
+        mFavouriteList = null;
+    }
+
+    private ListView mFavouriteList;
 }
