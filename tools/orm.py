f95f134486e898830b1cefe99985c6065c1ae2eb
John Bytheway <jbytheway@gmail.com>
2016-01-02 16:13:07 +0000

    Replace some string keys with integers
    The point of this is to make the stop_times table smaller, since that accounts
    for most of the database size.
    
    Unfortunately, compressed size is still 16MiB, which is more than I would
    prefer...

--- tools/orm.py
+++ tools/orm.py
@@ -25,12 +25,19 @@ class Stop(Mixin, Base):
     __tablename__ = 'stops'
 
     ignore_fields = ('stop_desc', 'stop_url', 'location_type', 'zone_id')
+    mapping = {}
 
-    stop_id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    stop_id = Column(String, unique=True)
     stop_code = Column(String, index=True)
     stop_name = Column(String)
     stop_lat = Column(Float)
     stop_lon = Column(Float)
+
+    def __init__(self, *, stop_id, **kargs):
+        id = len(Stop.mapping)
+        Stop.mapping[stop_id] = id
+        super().__init__(id=id, stop_id=stop_id, **kargs)
 
 class Route(Mixin, Base):
     __tablename__ = 'routes'
@@ -44,13 +51,19 @@ class Trip(Mixin, Base):
     __tablename__ = 'trips'
 
     ignore_fields = ()
+    mapping = {}
 
-    trip_id = Column(String, primary_key=True)
+    trip_id = Column(Integer, primary_key=True)
     route_id = Column(String, ForeignKey('routes.route_id'), index=True)
     service_id = Column(String)
     trip_headsign = Column(String)
     direction_id = Column(Integer)
     block_id = Column(Integer)
+
+    def __init__(self, *, trip_id, **kargs):
+        new_id = len(Trip.mapping)
+        Trip.mapping[trip_id] = new_id
+        super().__init__(trip_id=new_id, **kargs)
 
 class StopTime(Mixin, Base):
     __tablename__ = 'stop_times'
@@ -58,10 +71,15 @@ class StopTime(Mixin, Base):
     ignore_fields = ('departure_time')
 
     id = Column(Integer, primary_key=True)
-    trip_id = Column(String, ForeignKey('trips.trip_id'), index=True)
+    trip_id = Column(Integer, ForeignKey('trips.trip_id'), index=True)
     arrival_time = Column(String)
-    stop_id = Column(String, ForeignKey('stops.stop_id'), index=True)
+    stop_id = Column(Integer, ForeignKey('stops.id'), index=True)
     stop_sequence = Column(Integer)
     pickup_type = Column(Integer)
     drop_off_type = Column(Integer)
+
+    def __init__(self, *, trip_id, stop_id, **kargs):
+        trip_id = Trip.mapping[trip_id]
+        stop_id = Stop.mapping[stop_id]
+        super().__init__(trip_id=trip_id, stop_id=stop_id, **kargs)
 
