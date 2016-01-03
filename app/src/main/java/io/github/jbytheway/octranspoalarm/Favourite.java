e0854418dd21ab2de45dcf7e50cccffe5e04e7b2
John Bytheway <jbytheway@gmail.com>
2016-01-03 18:07:46 +0000

    Add Favourite default constructor
    This is required for Sugar to work properly

--- app/src/main/java/io/github/jbytheway/octranspoalarm/Favourite.java
+++ app/src/main/java/io/github/jbytheway/octranspoalarm/Favourite.java
@@ -3,6 +3,9 @@ package io.github.jbytheway.octranspoalarm;
 import com.orm.SugarRecord;
 
 public class Favourite extends SugarRecord {
+    public Favourite() {
+    }
+
     public Favourite(String name) {
         mName = name;
     }
