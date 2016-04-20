11206356f997198e4b06fbbdd532a0711a0eb99b
John Bytheway <jbytheway@gmail.com>
2016-04-19 23:48:27 -0400

    Use separate receivers for boot and internal
    For security reasons, we should check the action passed to my BroadcastReceiver
    handling boot events.  However, that means we can't use it internally.  So make
    two.

---
+++ app/src/main/java/io/github/jbytheway/rideottawa/BootReceiver.java
@@ -0,0 +1,18 @@
+package io.github.jbytheway.rideottawa;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class BootReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (!intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            return;
+        }
+
+        Intent intentToSend = new Intent(context, AlarmService.class);
+        intentToSend.putExtra(AlarmService.ACTION, AlarmService.ACTION_CHECK_ALARMS_WAKEFULLY);
+        startWakefulService(context, intentToSend);
+    }
+}
