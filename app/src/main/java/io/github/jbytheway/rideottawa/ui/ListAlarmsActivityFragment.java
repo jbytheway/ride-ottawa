6b0c20dd7a68dfdfcae710f0b8ff4e80092737cc
John Bytheway <jbytheway@gmail.com>
2016-04-23 21:38:15 -0400

    Support refreshing alarms

--- app/src/main/java/io/github/jbytheway/rideottawa/ui/ListAlarmsActivityFragment.java
+++ app/src/main/java/io/github/jbytheway/rideottawa/ui/ListAlarmsActivityFragment.java
@@ -2,19 +2,25 @@ package io.github.jbytheway.rideottawa.ui;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.common.collect.ImmutableList;
 
 import org.joda.time.DateTime;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import io.github.jbytheway.rideottawa.Alarm;
@@ -26,6 +32,11 @@ import io.github.jbytheway.rideottawa.RideOttawaApplication;
 import io.github.jbytheway.rideottawa.utils.IndirectArrayAdapter;
 
 public class ListAlarmsActivityFragment extends Fragment {
+    private static final int MINIMUM_REFRESH_SECONDS = 15;
+
+    public ListAlarmsActivityFragment() {
+        mAlarms = new ArrayList<>();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -72,11 +83,10 @@ public class ListAlarmsActivityFragment extends Fragment {
                 mAlarmAdapter.notifyDataSetChanged();
             }
         };
-        final ArrayList<AlarmWithId> alarms = new ArrayList<>();
 
         final DisplayTripHelper tripHelper = new DisplayTripHelper(getActivity());
 
-        createAlarms(alarms, listener, tripHelper);
+        createAlarms(listener, tripHelper);
 
         mAlarmAdapter = new IndirectArrayAdapter<>(
                 getActivity(),
@@ -84,7 +94,7 @@ public class ListAlarmsActivityFragment extends Fragment {
                 new IndirectArrayAdapter.ListGenerator<AlarmWithId>() {
                     @Override
                     public List<AlarmWithId> makeList() {
-                        return alarms;
+                        return mAlarms;
                     }
                 },
                 new IndirectArrayAdapter.ViewGenerator<AlarmWithId>() {
@@ -111,7 +121,7 @@ public class ListAlarmsActivityFragment extends Fragment {
                                 if (alarmData != null) {
                                     PendingAlarmData.delete(alarmData);
                                 }
-                                createAlarms(alarms, listener, tripHelper);
+                                createAlarms(listener, tripHelper);
                                 mAlarmAdapter.notifyDataSetChanged();
                             }
                         });
@@ -124,20 +134,78 @@ public class ListAlarmsActivityFragment extends Fragment {
         return view;
     }
 
-    private void createAlarms(ArrayList<AlarmWithId> alarms, Alarm.OnRefreshedListener listener, DisplayTripHelper tripHelper) {
-        alarms.clear();
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_view_alarms, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                refreshIfLateEnough(true);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void createAlarms(Alarm.OnRefreshedListener listener, DisplayTripHelper tripHelper) {
+        mAlarms.clear();
         for (PendingAlarmData alarmData : ImmutableList.copyOf(PendingAlarmData.findAll(PendingAlarmData.class))) {
             Alarm alarm = alarmData.makeAlarm(listener, mOcTranspo);
             if (alarm == null) {
                 alarmData.delete();
             } else {
-                alarms.add(new AlarmWithId(alarmData.getId(), alarm));
-                alarm.refreshTimeEstimate(false, getActivity(), mOcTranspo);
+                mAlarms.add(new AlarmWithId(alarmData.getId(), alarm));
                 tripHelper.addFavouriteStop(alarm.getFavouriteStop());
             }
         }
+
+        refresh();
+    }
+
+    private boolean refreshIfLateEnough(boolean showMessage) {
+        DateTime now = new DateTime();
+        if (now.minusSeconds(MINIMUM_REFRESH_SECONDS).isBefore(mLastRefresh)) {
+            // Too soon; we won't refresh yet
+            if (showMessage) {
+                Toast.makeText(getActivity(), getString(R.string.skipping_refresh_too_soon), Toast.LENGTH_LONG).show();
+            }
+            return false;
+        } else {
+            refresh();
+            return true;
+        }
+    }
+
+    private void refresh() {
+        // The process of updating the Forthcoming trips can take a while (maybe half
+        // a second or more depending on how many routes are involved.
+        // Therefore we want to do it on a background thread.
+
+        // First we check if we're already refreshing (very unlikely, because we force a fairly
+        // long delay between refreshes)
+        for (AlarmWithId alarmWithId : mAlarms) {
+            alarmWithId.Alarm.refreshTimeEstimate(false, getActivity(), mOcTranspo);
+        }
+
+        mLastRefresh = DateTime.now();
+
+        /* Leaving out auto-refreshing for now
+        // Whether we actually refreshed or not, trigger another refresh
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isResumed()) {
+                    refreshIfLateEnough(false);
+                }
+            }
+        }, AUTO_REFRESH_SECONDS * 1000);*/
     }
 
     private OcTranspoDataAccess mOcTranspo;
+    private ArrayList<AlarmWithId> mAlarms;
     private IndirectArrayAdapter<AlarmWithId> mAlarmAdapter;
+    private DateTime mLastRefresh;
 }
